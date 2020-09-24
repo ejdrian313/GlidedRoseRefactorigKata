74bdc2d0d90c02dd17d143816e4132b6255f9e31
Adrian Kujawski <adriankujawski313@gmail.com>
2020-09-24 22:01:25 +0200

    test refactor full coverage

--- src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -6,37 +6,23 @@ import org.junit.jupiter.api.Test
 internal class GildedRoseTest {
 
     @Test
-    fun nameIsUnchanged() {
-        val items = arrayOf(Item("foo", 0, 0))
-        val app = GildedRose(items)
-        app.updateQuality()
-        assertEquals("foo", app.items[0].name)
-    }
-
-    @Test
-    fun backStagePassesToaTAFKAL80ETConcertQuality40SellInMinus5UpdateWell() {
-        val items = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", -5, 40))
-        val app = GildedRose(items)
+    fun testBackStageQuality() {
+        val app = GildedRose(arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", -5, 40)))
         app.updateQuality()
         assertEquals(0, app.items[0].quality)
-        assertEquals(-6, app.items[0].sellIn)
     }
 
     @Test
-    fun agedBrieSellInMinus5quality40updateWell() {
-        val items = arrayOf(Item("Aged Brie", -5, 40))
-        val app = GildedRose(items)
+    fun testAgedBrieQualityWhenSellInLessThanZero() {
+        val app = GildedRose(arrayOf(Item("Aged Brie", -5, 40)))
         app.updateQuality()
         assertEquals(42, app.items[0].quality)
-        assertEquals(-6, app.items[0].sellIn)
     }
 
     @Test
-    fun elseItemSellInMinus5quality40updateWell() {
-        val items = arrayOf(Item("Test2", -5, 40))
-        val app = GildedRose(items)
+    fun testElseItemQuality() {
+        val app = GildedRose(arrayOf(Item("Foo bar", -5, 40)))
         app.updateQuality()
         assertEquals(38, app.items[0].quality)
-        assertEquals(-6, app.items[0].sellIn)
     }
 }
