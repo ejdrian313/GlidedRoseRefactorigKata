e6f1e50a66560264ae70d58010f8017cce6d4165
Adrian Kujawski <adriankujawski313@gmail.com>
2020-09-24 22:01:45 +0200

    positive flow not negation

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -4,17 +4,13 @@ class GildedRose(var items: Array<Item>) {
 
     fun updateQuality() {
         for (i in items.indices) {
-            val item = items[i];
+            val item = items[i]
 
-            if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (item.quality > 0 && item.name != "Sulfuras, Hand of Ragnaros") {
-                    item.quality = item.quality - 1
-                }
-            } else {
+            if (item.name == "Aged Brie" || item.name == "Backstage passes to a TAFKAL80ETC concert" || item.name == "Sulfuras, Hand of Ragnaros") {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1
 
-                    if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+                    if (item.name == "Backstage passes to a TAFKAL80ETC concert") {//Backstage pass
                         if (item.sellIn < 11 && item.quality < 50) {
                             item.quality = item.quality + 1
                         }
@@ -23,6 +19,10 @@ class GildedRose(var items: Array<Item>) {
                         }
                     }
                 }
+            } else {
+                if (item.quality > 0) {
+                    item.quality = item.quality - 1
+                }
             }
 
             if (item.name != "Sulfuras, Hand of Ragnaros") {
@@ -30,17 +30,19 @@ class GildedRose(var items: Array<Item>) {
             }
 
             if (item.sellIn < 0) {
-                if (item.name != "Aged Brie") {
-                    if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (item.quality > 0 && item.name != "Sulfuras, Hand of Ragnaros") {
-                            item.quality = item.quality - 1
-                        }
-                    } else {
-                        item.quality = item.quality - item.quality
-                    }
-                } else {
+                if (item.name == "Aged Brie") {
                     if (item.quality < 50) {
                         item.quality = item.quality + 1
+                    }
+                } else {
+                    if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+                        item.quality = item.quality - item.quality
+                    } else {
+                        if (item.name == "Sulfuras, Hand of Ragnaros") {
+                            //nothing
+                        } else if (item.quality > 0) {
+                            item.quality = item.quality - 1
+                        }
                     }
                 }
             }
