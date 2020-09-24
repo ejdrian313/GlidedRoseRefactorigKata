39f0507111a79c3ace66dbddf2b996670added6b
Adrian Kujawski <adriankujawski313@gmail.com>
2020-09-24 23:45:35 +0200

    create factory method, decorator item per case, update glidedRose

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -4,49 +4,37 @@ class GildedRose(var items: Array<Item>) {
 
     fun updateQuality() {
         for (i in items.indices) {
-            val item = items[i]
+            val item = ItemFactory.createItem(items[i])
 
-            if (item.name == "Aged Brie" || item.name == "Backstage passes to a TAFKAL80ETC concert" || item.name == "Sulfuras, Hand of Ragnaros") {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1
-
-                    if (item.name == "Backstage passes to a TAFKAL80ETC concert") {//Backstage pass
-                        if (item.sellIn < 11 && item.quality < 50) {
-                            item.quality = item.quality + 1
-                        }
-                        if (item.sellIn < 6 && item.quality < 50) {
-                            item.quality = item.quality + 1
-                        }
-                    }
+            if (item is AgedBrie || item is BackstagePass || item is Sulfuras) {
+                item.increaseQuality()
+                if (item is BackstagePass) {
+                    item.increaseQualitySpecial()
                 }
             } else {
-                if (item.quality > 0) {
-                    item.quality = item.quality - 1
-                }
+                item.decreaseQuality()
             }
 
-            if (item.name != "Sulfuras, Hand of Ragnaros") {
-                item.sellIn = item.sellIn - 1
+            if (item !is Sulfuras) {
+                item.decreaseSellIn()
             }
 
-            if (item.sellIn < 0) {
-                if (item.name == "Aged Brie") {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1
+            if (item.sellInLessThanZero()) {
+                when (item) {
+                    is AgedBrie -> {
+                        item.increaseQuality()
                     }
-                } else {
-                    if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-                        item.quality = item.quality - item.quality
-                    } else {
-                        if (item.name == "Sulfuras, Hand of Ragnaros") {
-                            //nothing
-                        } else if (item.quality > 0) {
-                            item.quality = item.quality - 1
-                        }
+                    is BackstagePass -> {
+                        item.decreaseQuality()
+                    }
+                    is Sulfuras -> {
+                        //nothing
+                    }
+                    else -> {
+                        item.decreaseQuality()
                     }
                 }
             }
         }
     }
 }
-
--- src/main/kotlin/com/gildedrose/Item.kt
+++ src/main/kotlin/com/gildedrose/Item.kt
@@ -5,3 +5,137 @@ open class Item(var name: String, var sellIn: Int, var quality: Int) {
         return this.name + ", " + this.sellIn + ", " + this.quality
     }
 }
+
+class ItemFactory {
+    companion object {
+        fun createItem(item: Item): Manageable {
+            return when (item.name) {
+                "Aged Brie" -> AgedBrie(item)
+                "Backstage passes to a TAFKAL80ETC concert" -> BackstagePass(item)
+                "Sulfuras, Hand of Ragnaros" -> Sulfuras(item)
+                else -> DefaultItem(item)
+            }
+        }
+    }
+}
+
+interface Manageable {
+    fun increaseQuality()
+    fun decreaseQuality()
+    fun decreaseSellIn()
+    fun sellInLessThanZero(): Boolean
+}
+
+class AgedBrie(private val item: Item) : Manageable {
+    private val _quality = Quality(item.quality)
+
+    override fun increaseQuality() {
+        item.quality = _quality.increase()
+    }
+
+    override fun decreaseQuality() {
+        item.quality = _quality.decrease()
+    }
+
+    override fun decreaseSellIn() {
+        --item.sellIn
+    }
+
+    override fun sellInLessThanZero() = item.sellIn < 0
+
+    override fun toString(): String {
+        return item.toString()
+    }
+}
+
+class BackstagePass(private val item: Item) : Manageable {
+    private val _quality = Quality(item.quality)
+
+    override fun increaseQuality() {
+        item.quality = _quality.increase()
+    }
+
+    override fun decreaseQuality() {
+        item.quality = 0
+    }
+
+    override fun decreaseSellIn() {
+        --item.sellIn
+    }
+
+    override fun sellInLessThanZero() = item.sellIn < 0
+
+    override fun toString(): String {
+        return item.toString()
+    }
+
+    fun increaseQualitySpecial() {
+        if (item.sellIn < 11) {
+            increaseQuality()
+        }
+        if (item.sellIn < 6) {
+            increaseQuality()
+        }
+    }
+}
+
+class Sulfuras(private val item: Item) : Manageable {
+    private val _quality = Quality(item.quality)
+
+
+    override fun increaseQuality() {
+        item.quality = _quality.increase()
+    }
+
+    override fun decreaseQuality() {
+        item.quality = _quality.decrease()
+    }
+
+    override fun decreaseSellIn() {
+        --item.sellIn
+    }
+
+    override fun sellInLessThanZero() = item.sellIn < 0
+
+    override fun toString(): String {
+        return item.toString()
+    }
+}
+
+class DefaultItem(private val item: Item) : Manageable {
+    private val _quality = Quality(item.quality)
+
+
+    override fun increaseQuality() {
+        item.quality = _quality.increase()
+    }
+
+    override fun decreaseQuality() {
+        item.quality = _quality.decrease()
+    }
+
+    override fun decreaseSellIn() {
+        --item.sellIn
+    }
+
+    override fun sellInLessThanZero() = item.sellIn < 0
+
+    override fun toString(): String {
+        return item.toString()
+    }
+}
+
+class Quality(private var value: Int) {
+
+    fun increase(): Int {
+        return if (value < 50) {
+            ++value
+        } else value
+    }
+
+    fun decrease(): Int {
+        return if (value > 0) {
+            --value
+        } else value
+    }
+}
